a129a89edcd8068cfc05d7db2967721fcd36e6d3
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-16 16:21:53 +0530

    Added: the Maximum of Three Integers

--- src/com/bridgelabz/MaxOf3Integers.java
+++ src/com/bridgelabz/MaxOf3Integers.java
@@ -3,13 +3,25 @@ package com.bridgelabz;
 /**
  * purpose - To simulate Generics Method
  * MaxOf#Integers implements an application that
- * gererates max. of three Integer value
- * @author  Sunil
- * @version  16.0
+ * generates max. of three Integer value
+ * @author Sunil
+ * @version 16.0
  * @since 16/08/2021
  */
 public class MaxOf3Integers {
+    // used Integer object and compareTo To test MaximumNumber
+    public static Integer compareIntegerMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer maximumNumber = firstNumber;
+        if (secondNumber.compareTo(maximumNumber) > 0)
+            maximumNumber = secondNumber;
+        if (thirdNumber.compareTo(maximumNumber) > 0)
+            maximumNumber = thirdNumber;
+        return maximumNumber;
+    }
+
+    //declered MAin Method
     public static void main(String[] args) {
         System.out.printf("Welcome to Generics Method Programming ..");
+        System.out.printf("the Maximum value of three Integers :\n " + compareIntegerMaximum(55, 40, 50));
     }
 }
