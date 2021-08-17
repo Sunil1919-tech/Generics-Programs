18cd7fad2f442e7d91183db28d548167dc8ceb21
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-17 15:10:11 +0530

    Added: maximum of Three String values

--- src/com/bridgelabz/GenericsPgm.java
+++ src/com/bridgelabz/GenericsPgm.java
@@ -10,28 +10,21 @@ package com.bridgelabz;
  * @since 16/08/2021
  */
 public class GenericsPgm {
-    // used Integer object and compareTo To test MaximumNumber
-    public static Integer compareIntegerMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer maximumNumber = firstNumber;
-        if (secondNumber.compareTo(maximumNumber) > 0)
-            maximumNumber = secondNumber;
-        if (thirdNumber.compareTo(maximumNumber) > 0)
-            maximumNumber = thirdNumber;
-        return maximumNumber;
+    // used Integer object and compareTo To test Maximum String
+    public static String campareStringMaximum(String firstWord, String secWord, String thirdWord) {
+        String maximumString = firstWord;
+        if (secWord.compareTo(maximumString) > 0)
+            maximumString = secWord;
+        if (thirdWord.compareTo(maximumString) > 0)
+            maximumString = thirdWord;
+        return maximumString;
     }
-    public static Float compareFloatMaximum(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        Float maximumNumber = firstNumber;
-        if (secondNumber.compareTo(maximumNumber) > 0)
-            maximumNumber = secondNumber;
-        if (thirdNumber.compareTo(maximumNumber) > 0)
-            maximumNumber = thirdNumber;
-        return maximumNumber;
-    }
-        //declered MAin Method
-    public static void main(String[] args) {
-            System.out.printf("Welcome to Generics Method Programming ..");
-            System.out.printf("\n the Maximum value of three Integers : " + compareIntegerMaximum(55, 40, 50));
-            System.out.printf("\n the Maximum value of three Floats: " + compareFloatMaximum(22.9f, 3.3f, 33.65746f));
 
-        }
+    /**
+     * declaring the main method to call another class
+     */
+    public static void main(String[] args) {
+        System.out.printf("Welcome to Generics Method Programming ..");
+        System.out.printf("\n the Maximum value of three Integers : " + campareStringMaximum("leoMessi", "zlatanIbrahimovic", "aguera"));
     }
+}
